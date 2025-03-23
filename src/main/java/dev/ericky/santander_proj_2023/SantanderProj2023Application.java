059b48540cff744668fdb144a294de073b291793
erickystn <xkyx@live.com>
2025-03-23 20:05:00 -0300

    Meu primeiro commit

---
+++ src/main/java/dev/ericky/santander_proj_2023/SantanderProj2023Application.java
@@ -0,0 +1,13 @@
+package dev.ericky.santander_proj_2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderProj2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderProj2023Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ericky/santander_proj_2023/SantanderProj2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ericky.santander_proj_2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderProj2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
