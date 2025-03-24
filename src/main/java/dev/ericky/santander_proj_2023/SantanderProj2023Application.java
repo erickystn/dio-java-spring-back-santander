bc0e58838a2500b28ec074e7ce12776555a9a507
erickystn <xkyx@live.com>
2025-03-24 03:35:45 -0300

    Bugfix CORS

--- src/main/java/dev/ericky/santander_proj_2023/SantanderProj2023Application.java
+++ src/main/java/dev/ericky/santander_proj_2023/SantanderProj2023Application.java
@@ -1,8 +1,11 @@
 package dev.ericky.santander_proj_2023;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class SantanderProj2023Application {
 
